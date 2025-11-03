2e3a1b4cb42d33deef82bae5405e1d008bb1cb11
Raul Ortega Talavera <ortegapublicity@gmail.com>
2025-11-03 19:54:05 -0400

    Update Contentful client configuration to use VITE environment variables

--- src/Utlits/contentfulClient.js
+++ src/Utlits/contentfulClient.js
@@ -1,8 +1,8 @@
 // src/Utils/contentfulClient.js
 const contentful = require('contentful'); 
 const client = contentful.createClient({
-  space: process.env.CONTENTFUL_SPACE_ID,
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  space: process.env.VITE_CONTENTFUL_SPACE_ID,
+  accessToken: process.env.VITE_CONTENTFUL_ACCESS_TOKEN,
   environment: 'master' 
 });
 export default client;
